287624eaa9b385c18daa703aa63e490d763de4b3
Cassia Sotolani <ca.sotolani@gmail.com>
2022-04-18 14:25:11 -0400

    Pegando informações do primeiro usuário

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Cassia Sotolani - Nutrição";
+
+var paciente = document.querySelector('#primeiro-paciente');
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+var imcPrimeiroPaciente = peso / (altura * altura);
+
+console.log(tdPeso);
+console.log(tdAltura);
+console.log(imcPrimeiroPaciente);
