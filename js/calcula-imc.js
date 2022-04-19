434538891fbaa8d000726db7fe12a3b87456892a
Cassia Sotolani <ca.sotolani@gmail.com>
2022-04-19 09:27:24 -0400

    Separando as function em arquivos para o código ficar mais organizado

---
+++ js/calcula-imc.js
@@ -0,0 +1,34 @@
+var titulo = document.querySelector('.titulo');
+titulo.textContent = "Cassia Sotolani - Nutrição";
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for(var i = 0; i < pacientes.length; i++) {
+    var tdPeso = pacientes[i].querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+
+    var tdAltura = pacientes[i].querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdImc = pacientes[i].querySelector('.info-imc');
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        pacientes[i].classList.add('paciente-invalido');
+    }
+
+    if(altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        pacientes[i].classList.add('paciente-invalido');
+    }
+
+    if(alturaEhValida && pesoEhValido) {
+        var imcPrimeiroPaciente = (peso / (altura * altura));
+        tdImc.textContent = imcPrimeiroPaciente.toFixed(2);
+    }
+}
